8966836ff601fb3f57f16bd288951a687b65754b
more8591 <vmoreno8591@gmail.com>
2020-08-24 03:19:01 -0400

    Add files via upload

---
+++ js/stylescript.js
@@ -0,0 +1,46 @@
+/*
+Developed by Victor Moreno
+This js doc effects the following:
+-text animation
+-
+*/
+
+$(document).ready(function() {
+
+    /////////////////////////////
+    /////BIG TEXT ANIMATION//////
+    /////////////////////////////
+    $(".slide").textSlider({
+        timeout: 1500,
+        slideTime: 10,
+        loop: 1
+    });
+
+    ///////////////////////////////
+    /////MENU TOGGLE ANIMATION////
+    /////////////////////////////
+    var b1 = $("#toggleOff");
+    waterFloat(b1,900,0,8);
+
+    $(".cb02, .cb03").hide();
+
+    $("#toggleOff").mouseenter(function(){
+        $(this).animate({
+            opacity: 0.70
+        });
+    }).mouseleave(function(){
+        $(this).animate({
+            opacity: 1
+        });
+    });
+
+    ///FADES OUT KNOW MORE BUTTON TO SHOW MENU///
+    $("#toggleOff").on("click", function(){
+        $(".circleTrans").delay(200).animate({
+            left: '-100%',
+        });
+        $(".cb02").delay(1000).fadeIn(2000);
+        $(".cb01, #bg, .toggleOff").delay(200).fadeOut();
+    });
+
+});
---
+++ js/text-slider.js
@@ -0,0 +1,70 @@
+(function ( $ ) {
+	$.fn.textSlider = function ( options ) {
+
+		/* Default settings */
+		var settings = $.extend(
+			{
+				timeout: 2000,
+				slideTime: 750
+			},
+			options
+		);
+
+		var nextItem;
+
+		var currentItem = 0;
+		var count = 0;
+
+		this.children('.slider-item').each(
+			function ()
+			{
+
+				$(this).addClass( 'slide-' + ( count ) )
+					.css(
+						{
+							//opacity:	   0,
+							paddingTop:	'0px',
+							paddingBottom: '0px'
+						}
+					);
+
+				$(this).hide();
+
+				count++;
+
+			}
+		);
+
+		function firstSlide ()
+		{
+
+			$('.slide-' + currentItem ).show().animate({opacity: 1 }, settings.slideTime);
+
+			setTimeout ( transition, settings.timeout );
+
+		}
+
+		function transition ()
+		{
+
+			nextItem = parseInt ( currentItem + 1 );
+
+			if ( nextItem >= count )
+				nextItem = 0;
+
+			$('.slide-' + currentItem ).animate({opacity: 0 }, settings.slideTime, function () {
+				$(this).hide();
+				$('.slide-' + nextItem ).animate({opacity: 1 }, settings.slideTime).slideDown();
+			});
+
+			currentItem = nextItem;
+
+			setTimeout ( transition, settings.timeout );
+
+		}
+
+		return firstSlide ();
+
+	};
+
+}( jQuery ));
