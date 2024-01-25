21022eed434ef5f43c728635bc88c1edf53903d5
Victor <vmoreno8591@gmail.com>
2024-01-25 08:02:21 -0500

    sqish menu bug

--- js/stylescript.js
+++ js/stylescript.js
@@ -67,7 +67,7 @@ $(document).ready(function() {
             left: '-100%',
         });
         //FADE IN CONTENT BLOCKS 1 - 3//
-        $(".cb02, .contentBlock, .scr-container, .hidethis").delay(1000).fadeIn(2000);
+        $(".cb02, .contentBlock, .scr-container").delay(1000).fadeIn(2000);
         $(".cb01, #bg, #toggleOff").delay(200).fadeOut();
         //AUTO START AT TOP//
         $(window).scrollTop(0);
