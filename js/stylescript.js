8a024c7f53d4df68db92899330f1971a0339c3e4
Victor <vmoreno8591@gmail.com>
2024-06-19 16:47:38 -0400

    Updating JS

--- js/stylescript.js
+++ js/stylescript.js
@@ -48,7 +48,7 @@ $(document).ready(function() {
     ///////////////////////////////
     /////MENU TOGGLE ANIMATION////
     /////////////////////////////
-    $(".cb02, .cb03, .hidethis").hide();
+    $(".cb02, .cb03, .cb04, .hidethis").hide();
 
     $("#toggleOff").mouseenter(function(){
         $(this).animate({
@@ -66,8 +66,8 @@ $(document).ready(function() {
         $(".circleTrans").delay(200).animate({
             left: '-100%',
         });
-        //FADE IN CONTENT BLOCKS 1 - 3//
-        $(".cb02, .contentBlock, .scr-container, .hidethis").delay(1000).fadeIn(2000);
+        //FADE IN CONTENT BLOCKS 1 - 4//
+        $(".cb02, .cb03, .cb04, .contentBlock, .scr-container, .hidethis").delay(1000).fadeIn(2000);
         $(".cb01, #bg, #toggleOff").delay(200).fadeOut();
         //AUTO START AT TOP//
         $(window).scrollTop(0);
