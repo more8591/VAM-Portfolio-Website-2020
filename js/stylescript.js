d1c3356cbb7362829855d2dbf5716f7c44352ac9
Victor <vmoreno8591@gmail.com>
2025-08-09 15:11:05 -0400

    Update website
    updated website workings

--- js/stylescript.js
+++ js/stylescript.js
@@ -1,3 +1,47 @@
+// Slide menu-container out if not scrolling, slide in on scroll
+(function() {
+  const menu = document.querySelector('.menu-container');
+  let scrollTimeout;
+  function showMenu() {
+    if (menu) menu.classList.remove('menu-slide-out');
+  }
+  function hideMenu() {
+    if (menu) menu.classList.add('menu-slide-out');
+  }
+  function onScroll() {
+    showMenu();
+    if (scrollTimeout) clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(hideMenu, 2000); // 2s after scroll stops
+  }
+  window.addEventListener('scroll', onScroll, {passive:true});
+  // Start hidden if not scrolling
+  hideMenu();
+})();
+// Menu auto-hide after 1 min inactivity, slide out, reappear on mousemove
+(function() {
+  const menu = document.querySelector('.menu-container');
+  let hideTimeout;
+  function showMenu() {
+    if (menu) {
+      menu.classList.remove('menu-hide');
+    }
+  }
+  function hideMenu() {
+    if (menu) {
+      menu.classList.add('menu-hide');
+    }
+  }
+  function resetHideTimer() {
+    showMenu();
+    if (hideTimeout) clearTimeout(hideTimeout);
+    hideTimeout = setTimeout(hideMenu, 60000);
+  }
+  // Show on any mousemove or touch
+  window.addEventListener('mousemove', resetHideTimer);
+  window.addEventListener('touchstart', resetHideTimer);
+  // Start timer on load
+  resetHideTimer();
+})();
 /*
 Developed by Victor Moreno
 This js doc effects the following:
@@ -35,9 +79,9 @@ $(document).ready(function() {
      var scrlnum = $(this).scrollTop();
      //////SCROLL DOWN MORE FLOAT/////
      if (scrlnum >=300) {
-      $(".scr-body").fadeOut();
+      $(".scr-body, .scr-container").fadeOut();
      } else {
-      $(".scr-body").fadeIn();
+      $(".scr-body, .scr-container").fadeIn();
      }
     })
     ////////////////////////////////////
@@ -48,7 +92,7 @@ $(document).ready(function() {
     ///////////////////////////////
     /////MENU TOGGLE ANIMATION////
     /////////////////////////////
-    $(".cb02, .cb03, .cb04, .hidethis").hide();
+    $(".cb02, .cb03, .cb04, .cb05, .hidethis").hide();
 
     $("#toggleOff").mouseenter(function(){
         $(this).animate({
@@ -67,7 +111,7 @@ $(document).ready(function() {
             left: '-100%',
         });
         //FADE IN CONTENT BLOCKS 1 - 4//
-        $(".cb02, .cb03, .cb04, .contentBlock, .scr-container, .hidethis").delay(1000).fadeIn(2000);
+        $(".cb02, .cb03, .cb04, .cb05, .contentBlock, .scr-container, .hidethis").delay(1000).fadeIn(2000);
         $(".cb01, #bg, #toggleOff").delay(200).fadeOut();
         //AUTO START AT TOP//
         $(window).scrollTop(0);
